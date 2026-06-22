d20fea8df685002366bdeb224d9c64980c8cdb0d
ma611444400-collab <ma611444400@gmail.com>
2026-06-22 09:34:49 +0300

    Add Color.kt for theme color definitions

---
+++ app/src/main/java/com/sahalsawir/app/model/app/src/main/java/com/sahalsawir/app/model/app/src/main/java/com/sahalsawir/app/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.sahalsawir.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF000000)
+val DarkSurface = Color(0xFF0A0A0A)
+val DarkSurfaceContainer = Color(0xFF171717)
+val PrimarySlate = Color(0xFFF1F5F9)
+val AccentTeal = Color(0xFF3B82F6)
+val PremiumGold = Color(0xFF3B82F6)
+val AccentBlue = Color(0xFF2563EB)
+val TextPrimary = Color(0xFFFAFAFA)
+val TextSecondary = Color(0xFF737373)
+val OutlineBorder = Color(0xFF262626)
+val GlassOverlay = Color(0x990A0A0A)
